a37fc1926d604ec04ed7ce5acc282e155e7f6cba
Smitkhobragade <100017129+Smitkhobragade@users.noreply.github.com>
2023-03-24 12:20:33 +0530

    minor changes

--- dynamc_stack.c
+++ dynamc_stack.c
@@ -2,87 +2,99 @@
 #include <stdlib.h>
 #define CAPACITY 10
 
-
-typedef struct stack{
+typedef struct stack
+{
 	int *s;
 	int top;
 	int maxsize;
-}stack;
+} stack;
 
-void extendstack(stack * s1){
-	s1 -> s = (int *)realloc(s1->s,(((s1->maxsize)*1.5)*sizeof(int)));
-	//s1 -> maxsize = ((s1 -> maxsize) * 3)/2;
+void extendstack(stack *s1)
+{
+	s1->s = (int *)realloc(s1->s, (((s1->maxsize) * 1.5) * sizeof(int)));
+	// s1 -> maxsize = ((s1 -> maxsize) * 3)/2;
 }
 
-
-int isempty(stack s1){
-	if(s1.top == -1)
+int isempty(stack s1)
+{
+	if (s1.top == -1)
 		return 1;
 	else
 		return 0;
 }
 
-int isfull(stack * s1,int max){
-	if(s1->top == max-1){
+int isfull(stack *s1, int max)
+{
+	if (s1->top == max - 1)
+	{
 		printf("The stack is now full extending the limit\n");
 		extendstack(s1);
-		printf("Extended the stack now\nWith Capacity: %d\n",((s1->maxsize)*3)/2);
+		printf("Extended the stack now\nWith Capacity: %d\n", ((s1->maxsize) * 3) / 2);
 		return 1;
 	}
 	else
 		return 0;
 }
 
-void push(stack * s1,int data){
-	isfull(s1,s1->maxsize);
-	
+void push(stack *s1, int data)
+{
+	isfull(s1, s1->maxsize);
+
 	(s1->top)++;
 	s1->s[s1->top] = data;
 }
 
-void create(stack *s1, int max){
-	s1 -> maxsize = max;
-	s1 -> s = (int *)malloc((s1->maxsize)*sizeof(int));
-	s1 -> top = -1;
+void create(stack *s1, int max)
+{
+	s1->maxsize = max;
+	s1->s = (int *)malloc((s1->maxsize) * sizeof(int));
+	s1->top = -1;
 }
 
-
-int peek(stack s1){
-	if(isempty(s1)){
+int peek(stack s1)
+{
+	if (isempty(s1))
+	{
 		printf("Stack is empty (Underflow)\n");
 		return 0;
 	}
-	else{
-		printf("The element is : %d\n",s1.s[s1.top]);
+	else
+	{
+		printf("The element is : %d\n", s1.s[s1.top]);
 		return s1.s[s1.top];
 	}
 }
 
-
-int pop(stack *s1){
-	if(isempty(*s1)==1){
+int pop(stack *s1)
+{
+	if (isempty(*s1) == 1)
+	{
 		printf("Cannot pop the element (stack underflow)\n");
 	}
-	else{
+	else
+	{
 		int data = s1->s[s1->top];
 		(s1->top)--;
 		return data;
 	}
 }
 
-
-int main(){
+int main()
+{
 	stack stk;
-	create(&stk,CAPACITY);
+	create(&stk, CAPACITY);
 	peek(stk);
-	for(int i=0;i<15;i++){
-	push(&stk,i);
-	peek(stk);	}
-	push(&stk,2);
-	push(&stk,10);
-	for(int i =0;i<15;i++){
-	pop(&stk);
-	peek(stk);
+	for (int i = 0; i < 15; i++)
+	{
+		push(&stk, i);
+		peek(stk);
+	}
+	push(&stk, 2);
+	push(&stk, 10);
+	for (int i = 0; i < 15; i++)
+	{
+		pop(&stk);
+		peek(stk);
 	}
 	pop(&stk);
 	peek(stk);
@@ -90,8 +102,5 @@ int main(){
 	pop(&stk);
 	peek(stk);
 
-
 	return 0;
 }
-
-
